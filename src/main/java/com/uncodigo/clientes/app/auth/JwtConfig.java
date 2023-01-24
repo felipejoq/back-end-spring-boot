ad73f7ab4bf37559a66a5ee6a3009ad6e6ddac48
felipejoq <felipejofreq@gmail.com>
2023-01-24 20:28:46 -0300

    Corrigiendo errores por versión de Spring Boot

---
+++ src/main/java/com/uncodigo/clientes/app/auth/JwtConfig.java
@@ -0,0 +1,5 @@
+package com.uncodigo.clientes.app.auth;
+
+public class JwtConfig {
+    public static final String SECRET_KEY = "has-verifica-token-14215";
+}
--- src/main/java/com/uncodigo/clientes/app/auth/SpringSecurityConfig.java
+++ src/main/java/com/uncodigo/clientes/app/auth/SpringSecurityConfig.java
@@ -5,16 +5,16 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.web.SecurityFilterChain;
 
+@EnableGlobalMethodSecurity(securedEnabled=true)
 @Configuration
-public class SpringSecurityConfig extends WebSecurityConfiguration {
-
+public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     private UserDetailsService usuarioService;
 
@@ -23,20 +23,25 @@ public class SpringSecurityConfig extends WebSecurityConfiguration {
         return new BCryptPasswordEncoder();
     }
 
-    @Bean
-    protected AuthenticationManager authenticationManager(AuthenticationManagerBuilder auth) throws Exception {
-        return auth.userDetailsService(usuarioService).passwordEncoder(passwordEncoder()).and().build();
+    @Override
+    @Autowired
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(this.usuarioService).passwordEncoder(passwordEncoder());
     }
 
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    @Bean("authenticationManager")
+    @Override
+    protected AuthenticationManager authenticationManager() throws Exception {
+        return super.authenticationManager();
+    }
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
                 .anyRequest().authenticated()
                 .and()
                 .csrf().disable()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-
-        return http.build();
     }
 
 }
